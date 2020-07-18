a035d57f7df451a558b21c91b5f81b49191acd4a
Sam Tang <xu.yue09@hotmail.com>
2020-07-17 23:39:49 -0600

    React Project Part 1

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './Components/MenuComponent';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
+    );
+  }
+}
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +36,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
